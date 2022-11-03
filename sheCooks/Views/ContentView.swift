def961f3dfa0c4df6860729e2895d2cae6081e30
britanyflores <bflores@cpp.edu>
2022-11-02 17:31:38 -0700

    Instructions

---
+++ sheCooks/Views/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift or Home Screen
+//  this is where the ingredients will be
+//  sheCooks
+//
+//  Created by Britany Flores on 10/19/22.
+
+
+
+import SwiftUI
+import CoreData
+
+
+
+struct ContentView: View{
+
+    var body: some View {
+        Text("Hello")
+
+    }
+
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+}
